cfe466219a564a749b942f3255f31423f95da5b9
Johnny van de Laar <j.vandelaar@pararius.nl>
2020-11-26 13:28:42 +0100

    Config fixes

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,5 +1,8 @@
 import React from "react";
 import CMS from "netlify-cms-app";
+import { nl } from 'netlify-cms-locales';
+
+CMS.registerLocale('nl', nl);
 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
