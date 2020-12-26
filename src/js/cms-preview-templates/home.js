91b4a1788f9cb227d7c11dba5bc332c0800921d4
Johnny van de Laar <j.vandelaar@pararius.nl>
2020-12-26 14:14:22 +0100

    home preview

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -1,30 +1,28 @@
 import React from "react";
-import format from "date-fns/format";
 
 export default class PostPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
-    const imageUrl = '/files/header_dames.jpg';
     const title = entry.getIn(["data", "blurb", "heading"]);
 
-    let titleFormatted = '<div className="f4-ns f5 b di lh-title mb3 white mw6 pa1"></div>';
+    let titleFormatted = <div className="f4-ns f5 b di lh-title mb3 white mw6 pa1"></div>;
     if (title) {
-      titleFormatted = '<h1 className="f4-ns f5 b di lh-title mb3 white mw6 bg-primary pa1">' + title + '</h1>';
+      titleFormatted = <h1 className="f4-ns f5 b di lh-title mb3 white mw6 bg-primary pa1">{title}</h1>;
     }
 
     const subtitle = entry.getIn(["data", "blurb", "text"]);
     let subtitleFormatted = '';
     if (subtitle) {
-      subtitleFormatted = '<p className="b f6 di lh-title mb3 white mw6 bg-primary pa1">' + subtitle + '</p>';
+      subtitleFormatted = <p className="b f6 di lh-title mb3 white mw6 bg-primary pa1">{subtitle}</p>;
     }
 
-    return <div className="pv4 pv5-l ph3 bg-center cover w-100 br1" style="background-image: url('{imageUrl}')">
+    return <div className="pv4 pv5-l ph3 bg-center cover w-100 br1" style="background-image: url('/files/header_dames.jpg')">
       <div className="mw7 right-1 ph3">
         <div className="db mb3">
           <div className="mw7 relative bg-fix-primary mb3">{titleFormatted}</div>
           <div className="mw7 relative bg-fix-primary">{subtitleFormatted}</div>
         </div>
       </div>
-    </div>
+    </div>;
   }
 }
