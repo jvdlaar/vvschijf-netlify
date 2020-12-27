059f5bf90d148653b5de1fe6a32b8db304ec2431
Johnny van de Laar <j.vandelaar@pararius.nl>
2020-12-27 12:12:38 +0100

    home preview

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -1,26 +1,30 @@
 import React from "react";
 
+const HomeTitle = ({data}) => data
+  ? <h1 className="f4-ns f5 b di lh-title mb3 white mw6 bg-primary pa1">{data}</h1>
+  : <div className="f4-ns f5 b di lh-title mb3 white mw6 pa1"></div>
+;
+
+const HomeSubTitle = ({data}) => data
+  ? <p className="b f6 di lh-title mb3 white mw6 bg-primary pa1">{data}</p>
+  : ''
+;
+
 export default class PostPreview extends React.Component {
   render() {
-    const {entry, getAsset} = this.props;
+    const {entry} = this.props;
     const title = entry.getIn(["data", "blurb", "heading"]);
-
-    let titleFormatted = <div className="f4-ns f5 b di lh-title mb3 white mw6 pa1"></div>;
-    if (title) {
-      titleFormatted = <h1 className="f4-ns f5 b di lh-title mb3 white mw6 bg-primary pa1">{title}</h1>;
-    }
-
     const subtitle = entry.getIn(["data", "blurb", "text"]);
-    let subtitleFormatted = '';
-    if (subtitle) {
-      subtitleFormatted = <p className="b f6 di lh-title mb3 white mw6 bg-primary pa1">{subtitle}</p>;
-    }
 
     return <div className="pv4 pv5-l ph3 bg-center cover w-100 br1" style="background-image: url('/files/header_dames.jpg')">
       <div className="mw7 right-1 ph3">
         <div className="db mb3">
-          <div className="mw7 relative bg-fix-primary mb3">{titleFormatted}</div>
-          <div className="mw7 relative bg-fix-primary">{subtitleFormatted}</div>
+          <div className="mw7 relative bg-fix-primary mb3">
+            <HomeTitle data={title} />
+          </div>
+          <div className="mw7 relative bg-fix-primary">
+            <HomeSubTitle data={subtitle} />
+          </div>
         </div>
       </div>
     </div>;
