79da91605ad779080cfe2a44a5aee712e6f2e6d6
Johnny van de Laar <j.vandelaar@pararius.nl>
2020-12-31 16:15:13 +0100

    home + page preview

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -1,34 +1,32 @@
 import React from "react";
 
-// const HomeTitle = ({title}) => title
-//   ? <h1 className="f4-ns f5 b di lh-title mb3 white mw6 bg-primary pa1">{title}</h1>
-//   : <div className="f4-ns f5 b di lh-title mb3 white mw6 pa1"></div>
-// ;
-//
-// const HomeSubTitle = ({subTitle}) => subTitle
-//   ? <p className="b f6 di lh-title mb3 white mw6 bg-primary pa1">{subTitle}</p>
-//   : ''
-// ;
+const HomeTitle = ({title}) => title
+  ? <h1 className="f4-ns f5 b di lh-title mb3 white mw6 bg-primary pa1">{title}</h1>
+  : <div className="f4-ns f5 b di lh-title mb3 white mw6 pa1"></div>
+;
+
+const HomeSubTitle = ({subTitle}) => subTitle
+  ? <p className="b f6 di lh-title mb3 white mw6 bg-primary pa1">{subTitle}</p>
+  : ''
+;
 
 export default class HomePreview extends React.Component {
   render() {
-    // const {entry} = this.props;
-    // const title = entry.getIn(["data", "blurb", "heading"]);
-    // const subtitle = entry.getIn(["data", "blurb", "text"]);
+    const {entry} = this.props;
+    const title = entry.getIn(["data", "blurb", "heading"]);
+    const subtitle = entry.getIn(["data", "blurb", "text"]);
 
-    // return <div className="pv4 pv5-l ph3 bg-center cover w-100 br1" style="background-image: url('/files/header_dames.jpg')">
-    //   <div className="mw7 right-1 ph3">
-    //     <div className="db mb3">
-    //       <div className="mw7 relative bg-fix-primary mb3">
-    //         {/*<HomeTitle title={title} />*/}
-    //       </div>
-    //       <div className="mw7 relative bg-fix-primary">
-    //         {/*<HomeSubTitle subTitle={subtitle} />*/}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>;
-
-    return "HOME";
+    return <div className="pv4 pv5-l ph3 bg-center cover w-100 br1" style="background-image: url('/files/header_dames.jpg')">
+      <div className="mw7 right-1 ph3">
+        <div className="db mb3">
+          <div className="mw7 relative bg-fix-primary mb3">
+            <HomeTitle title={title} />
+          </div>
+          <div className="mw7 relative bg-fix-primary">
+            <HomeSubTitle subTitle={subtitle} />
+          </div>
+        </div>
+      </div>
+    </div>;
   }
 }
