c08d87b803c605a141c8283247ff0db8cb748996
Johnny van de Laar <j.vandelaar@pararius.nl>
2021-01-08 18:39:27 +0100

    contact title preview

--- src/js/cms-preview-templates/contact.js
+++ src/js/cms-preview-templates/contact.js
@@ -1,6 +1,18 @@
 import React from "react";
 import {markdownToHtml} from "netlify-cms-widget-markdown/dist/esm/serializers";
 
+const HeadTitle = ({title}) => <div className="pv4 pv5-l ph3 bg-center cover w-100 br1 header-dames">
+    <div className="mw7 right-1 ph3">
+      <div className="db mb3">
+        <div className="mw7 relative bg-fix-primary mb3">
+          <h1 className="f4-ns f5 b di lh-title mb3 white mw6 bg-primary pa1">{title}</h1>
+        </div>
+        <div className="mw7 relative bg-fix-primary"></div>
+      </div>
+    </div>
+  </div>
+;
+
 const ContactEntry = ({heading, text, telephone, email}) =>
   <div className="ph2-ns w-50-ns mb4">
     <h4 className="f4 b lh-title mb2 primary">{ heading }</h4>
@@ -26,11 +38,14 @@ export default class ContactPreview extends React.Component {
     const {entry, getAsset, widgetFor} = this.props;
     const entryContactEntries = entry.getIn(["data", "contact_entries"]);
     const contactEntries = entryContactEntries ? entryContactEntries.toJS() : [];
-    return <div className="ph3 bg-off-white">
-      <div className="center mw6 pv3">
-        <img src={getAsset(entry.getIn(["data", "logo"]))} alt="" className="db center mb3" />
-        <ContactEntries data={contactEntries} />
-        { widgetFor("body") }
+    return <div>
+      <HeadTitle title={entry.getIn(["data", "title"])} />
+      <div className="ph3 bg-off-white">
+        <div className="center mw6 pv3">
+          <img src={getAsset(entry.getIn(["data", "logo"]))} alt="" className="db center mb3" />
+          <ContactEntries data={contactEntries} />
+          { widgetFor("body") }
+        </div>
       </div>
     </div>;
   }
