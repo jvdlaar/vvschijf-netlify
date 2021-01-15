c7cbb3ce7305fad018dd26bc9a2e443dff5600b9
Johnny van de Laar <j.vandelaar@pararius.nl>
2021-01-15 17:43:43 +0100

    images debug

--- src/js/cms-preview-templates/page.js
+++ src/js/cms-preview-templates/page.js
@@ -19,7 +19,8 @@ const PageDate = ({date}) => (date
 ;
 
 const PageHeadImage = ({image}) => (image ? <img src={image} /> : "");
-const PageImage = ({image}) => <div class="ph2-ns w-50-ns"><img src={image} alt="" class="center db mb3" style="width: 240px" /></div>;
+// const PageImage = ({image}) => <div class="ph2-ns w-50-ns"><img src={image} alt="" class="center db mb3" style="width: 240px" /></div>;
+const PageImage = ({image}) => image;
 
 export default class PostPreview extends React.Component {
   render() {
