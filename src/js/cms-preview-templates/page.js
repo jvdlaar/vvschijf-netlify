7d75afb9335b3ad452862f88a161edfb11b4e7ce
Johnny van de Laar <j.vandelaar@pararius.nl>
2021-01-15 15:14:36 +0100

    images debug

--- src/js/cms-preview-templates/page.js
+++ src/js/cms-preview-templates/page.js
@@ -25,7 +25,7 @@ export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset, widgetsFor} = this.props;
     const image = entry.getIn(["data", "image"]);
-    const images = widgetsFor("images").map((image) => <PageImage image={getAsset(image.getIn(["data", "image"]))} />);
+    // const images = widgetsFor("images").map((image) => <PageImage image={getAsset(image.getIn(["data", "image"]))} />);
 
     return <div>
       <HeadTitle title={entry.getIn(["data", "title"])} />
